7c9f23df80310aba6c1384e86610d6d4b6d15ff9
LedmaxPC <xalidzeyxur@gmail.com>
2025-12-19 16:11:22 +0400

    Initial commit - Xalid's Portfolio

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,11 +9,10 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi everyone! I’m <span className="purple">Xalid Shikhverdiyev</span>{" "}
-            from <span className="purple">Bhubaneswar, India</span>.
+            from <span className="purple">Baku , Azerbaijan</span>.
             <br />
             I’m currently working as a{" "}
-            <span className="purple">Backend Developer</span> at{" "}
-            <span className="purple">Juspay</span>.
+            <span className="purple">Backend Developer.</span> at{" "}
             <br />I hold an Integrated M.Sc. (IMSc) in{" "}
             <span className="purple">Mathematics and Computing</span> from{" "}
             <span className="purple">BIT Mesra</span>.
@@ -38,7 +37,7 @@ function AboutCard() {
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">XalidSh</footer>
         </blockquote>
       </Card.Body>
     </Card>
